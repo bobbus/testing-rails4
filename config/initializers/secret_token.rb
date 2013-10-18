1e605066564976eaaece910b72c9db1ac9f824f3
Adrien Coquio <adrien.coquio@gmail.com>
2013-10-18 14:16:49 +0200

    Rails 4 app with pg setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testing4::Application.config.secret_key_base = 'aadd1174485c86d19cf2f90ec80b1aaa2e6efefce23d3733001a643dce754cd7b47e041806fa765337349f6f74fe1f23a53d7058147f368916009e2e29decd1e'
