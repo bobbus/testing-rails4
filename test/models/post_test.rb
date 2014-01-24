6176b0d76f910f6f3ea1f8b389a71e23f8b0f4a2
Adrien Coquio <adrien.coquio@gmail.com>
2014-01-24 14:33:27 +0100

    Add comment on test file

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,12 +1,19 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
+  # PASS
   test "setting image" do
     subject = Post.new
     filepath = Rails.root.join('test','fixtures', 'fox.png')
     subject.image = filepath
     assert_equal 'fox.png', subject.image.name
   end
+
+  # FAIL
+  # PostTest#test_setting_image_and_thumb [/home/bob/doc/dev/test-apps/testing4/test/models/post_test.rb:22]:
+  # Expected: "fox.png"
+  #   Actual: "fox.jpg"
+
   test "setting image and thumb" do
     subject = Post.new
     filepath = Rails.root.join('test','fixtures', 'fox.png')
